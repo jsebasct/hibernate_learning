28095c674985e232bf4f3902d56d87ccff02e3ba
juan sebastian cruz torrico <jsebas.ct@gmail.com>
2017-07-27 15:26:45 -0300

    feat: recovering objects with get

--- src/main/java/people/hiber/App.java
+++ src/main/java/people/hiber/App.java
@@ -1,8 +1,11 @@
 package people.hiber;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import people.hiber.dto.UserDetails;
 
@@ -15,24 +18,35 @@ public class App
     public static void main( String[] args )
     {
     	UserDetails ud = new UserDetails();
-    	ud.setUserId(1);
-    	ud.setUserName("Juan");
-    	
+    	ud.setUserId(2);
+    	ud.setUserName("Pedro");
+    	ud.setAddress("Av Belgrano 955");
+    	ud.setJoinedDate(new Date());
+    	ud.setDescription("some descritpion");
     	// create a session factory
-    	SessionFactory sf = new Configuration().configure().buildSessionFactory();
+    	SessionFactory factory;// = new Configuration().configure().buildSessionFactory();
     	
-//    	sf = new MetadataSources( 
-//    			new StandardServiceRegistryBuilder()
-//    				.configure("hibernate.cfg.xml").build()
-//        	).buildMetadata().buildSessionFactory();
+    	factory = new MetadataSources( 
+    			new StandardServiceRegistryBuilder()
+    				.configure("hibernate.cfg.xml").build()
+        	).buildMetadata().buildSessionFactory();
     	
     	
-    	Session session = sf.openSession();
+    	Session session = factory.openSession();
     	
     	// transaction
     	session.beginTransaction();
     	session.save(ud);
     	session.getTransaction().commit();
+    	//sf.close();
+    	
+    	Session ss = factory.openSession();
+    	ss.beginTransaction();
+    	UserDetails user = ss.get(UserDetails.class, 2);
+    	System.out.println( "user recovered: " + user.getUserName() );
+    	
+    	ss.getTransaction().commit();
+    	ss.close();
     	
         System.out.println( "Finsh!" );
     }
